704fdb1d64b6d6715639b6071c41855a38495689
Harvey Sun <harveysuncn@163.com>
2023-03-10 10:25:03 +0800

    fixing namespace

--- include/log/log.hpp
+++ include/log/log.hpp
@@ -11,11 +11,11 @@
 
 namespace sheep {
 
-namespace dagger
+namespace log
 {
   using HashFunc = std::hash<std::thread::id>;
   static HashFunc Hasher = HashFunc();
-} // namespace dagger
+} // namespace log
 
 }
 
@@ -35,36 +35,36 @@ namespace dagger
 #define LOG_FILE() __FILENAME__
 #define LOG_LINE() __LINE__
 #define GET_TID() \
-  dagger::Hasher(std::this_thread::get_id()) // std::thread::id -> std::size_t
+  log::Hasher(std::this_thread::get_id()) // std::thread::id -> std::size_t
 
 #define trace(format, ...)                                                 \
-  log(dagger::TRACE,                                            \
+  log(log::TRACE,                                            \
       FMT_STRING("{:%Y-%m-%d %H:%M:}{:%S} {} {} [{}:{}@{}] " format "\n"), \
-      logLevelString(dagger::TRACE), GET_TID(), LOG_FUNC(), LOG_FILE(),    \
+      logLevelString(log::TRACE), GET_TID(), LOG_FUNC(), LOG_FILE(),    \
       LOG_LINE(), ##__VA_ARGS__)
 
 #define debug(format, ...)                                                 \
-  log(dagger::DEBUG,                                            \
+  log(log::DEBUG,                                            \
       FMT_STRING("{:%Y-%m-%d %H:%M:}{:%S} {} {} [{}:{}@{}] " format "\n"), \
-      logLevelString(dagger::DEBUG), GET_TID(), LOG_FUNC(), LOG_FILE(),    \
+      logLevelString(log::DEBUG), GET_TID(), LOG_FUNC(), LOG_FILE(),    \
       LOG_LINE(), ##__VA_ARGS__)
 
 #define info(format, ...)                                                  \
-  log(dagger::INFO,                                             \
+  log(log::INFO,                                             \
       FMT_STRING("{:%Y-%m-%d %H:%M:}{:%S} {} {} [{}:{}@{}] " format "\n"), \
-      logLevelString(dagger::INFO), GET_TID(), LOG_FUNC(), LOG_FILE(),     \
+      logLevelString(log::INFO), GET_TID(), LOG_FUNC(), LOG_FILE(),     \
       LOG_LINE(), ##__VA_ARGS__)
 
 #define warn(format, ...)                                                  \
-  log(dagger::WARN,                                             \
+  log(log::WARN,                                             \
       FMT_STRING("{:%Y-%m-%d %H:%M:}{:%S} {} {} [{}:{}@{}] " format "\n"), \
-      logLevelString(dagger::WARN), GET_TID(), LOG_FUNC(), LOG_FILE(),     \
+      logLevelString(log::WARN), GET_TID(), LOG_FUNC(), LOG_FILE(),     \
       LOG_LINE(), ##__VA_ARGS__)
 
 #define error(format, ...)                                                 \
-  log(dagger::ERROR,                                            \
+  log(log::ERROR,                                            \
       FMT_STRING("{:%Y-%m-%d %H:%M:}{:%S} {} {} [{}:{}@{}] " format "\n"), \
-      logLevelString(dagger::ERROR), GET_TID(), LOG_FUNC(), LOG_FILE(),    \
+      logLevelString(log::ERROR), GET_TID(), LOG_FUNC(), LOG_FILE(),    \
       LOG_LINE(), ##__VA_ARGS__)
 
 #define sleepus(microSec) \
